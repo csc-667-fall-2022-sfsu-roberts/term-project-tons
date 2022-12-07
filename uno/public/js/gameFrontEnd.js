4998e01360d11574448db312c381c21cfd8bd7aa
Nathan Rennacker <nrennacker@sfsu.mail.edu>
2022-12-06 16:33:47 -0800

    opponent front end order with names

--- uno/public/js/gameFrontEnd.js
+++ uno/public/js/gameFrontEnd.js
@@ -6,47 +6,47 @@ const message_container = document.querySelector('.chat-field');
 const messageButton = document.querySelector('.input-button');
 const input = document.querySelector('.input-field-chat');
 const wildcardButtonContainer = document.querySelector(
-  '.wildcardButtonContainer'
+    '.wildcardButtonContainer'
 );
 
 const socket = io({
-  path: '/games/',
-  query: {
-    game_id: gameId,
-  },
+    path: '/games/',
+    query: {
+        game_id: gameId,
+    },
 });
 
 // GCHAT functions
 messageButton.addEventListener('click', addMessage);
 socket.on('chat_message', (data) => {
-  message_container.innerHTML += createContainer(data.username, data.message);
+    message_container.innerHTML += createContainer(data.username, data.message);
 });
 
 function addMessage() {
-  if (input.value === '') {
-    return;
-  } else {
-    var message = {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      credentials: 'include',
-      body: JSON.stringify({
-        message: input.value,
-      }),
-    };
-    fetch(`/api/games/${gameId}/chat`, message).catch((err) =>
-      console.log(err)
-    );
-    input.value = '';
-    input.focus();
-  }
+    if (input.value === '') {
+        return;
+    } else {
+        var message = {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            credentials: 'include',
+            body: JSON.stringify({
+                message: input.value,
+            }),
+        };
+        fetch(`/api/games/${gameId}/chat`, message).catch((err) =>
+            console.log(err)
+        );
+        input.value = '';
+        input.focus();
+    }
 }
 
 function createContainer(username, message) {
-  return `
+    return `
   <div class="row comments mb-2">
   <div class="col-md-2 col-sm-2 col-3 text-center user-img">
   <p>&nbsp</p>
@@ -63,11 +63,17 @@ function createContainer(username, message) {
 }
 
 //  GAME STATE
-const gamePlayers = {
-  leftOpponent: '',
-  topOpponent: '',
-  rightOpponent: '',
-};
+
+let gameStarted = false;
+
+let opponentMap = new Map();
+
+
+const opponentLocations = {
+    leftOpponent: document.getElementById("leftOpponent"),
+    topOpponent: document.getElementById("topOpponent"),
+    rightOpponent: document.getElementById("rightOpponent"),
+}
 
 let discardPileDegree = [];
 for (let i = 0; i < 108; i++) {
@@ -76,31 +82,36 @@ for (let i = 0; i < 108; i++) {
 
 let currentUser;
 fetch('/api/users/current').then((response) => {
-  if (response.status == 200) {
-    response.json().then((data) => {
-      currentUser = data.user;
-      console.log(currentUser);
-    });
-  } else {
-    alert('Not logged in');
-  }
+    if (response.status == 200) {
+        response.json().then((data) => {
+            currentUser = data.user;
+            console.log(currentUser);
+        });
+    } else {
+        alert('Not logged in');
+    }
 });
 
 
 socket.on("game_state", (gameState) => {
     console.log(gameState)
-    //small issue with play order, if the player refreshes then the
+    gameStarted = gameState.started;
     const currentOpponents = gameState?.users.filter(opponent => {
         return opponent.user_id !== currentUser.user_id;
     }).sort((a, b) => a.play_order - b.play_order);
 
-    for (let opponent of currentOpponents) {
-        if (!gamePlayers[opponent]) {
-            Object.keys(gamePlayers).forEach(key => {
-                if (gamePlayers[key] === "") {
-                    gamePlayers[key] = opponent;
-                }
-            });
+    for (const opponent of currentOpponents){
+        if(opponentMap.get(opponent.user_id) === undefined){
+            if (!Array.from(opponentMap.values()).includes("leftOpponent")){
+                opponentMap.set(opponent.user_id,"leftOpponent");
+                showOpponent("leftOpponent", opponent.username);
+            } else if (!Array.from(opponentMap.values()).includes("topOpponent")){
+                opponentMap.set(opponent.user_id,"topOpponent");
+                showOpponent("topOpponent", opponent.username);
+            } else if (!Array.from(opponentMap.values()).includes("rightOpponent")){
+                opponentMap.set(opponent.user_id,"rightOpponent");
+                showOpponent("rightOpponent", opponent.username);
+            }
         }
     }
 
@@ -110,24 +121,30 @@ socket.on("game_state", (gameState) => {
     while (deleteAssets.firstChild) {
         deleteAssets.removeChild(deleteAssets.firstChild);
     }
+
     const currentUserCards = gameState?.cards.filter(card => {
         return card.user_id === currentUser.user_id;
     }).sort((a, b) => a.order - b.order);
+    let numCards = 0;
+    let root = document.querySelector(':root');
     for (const card of currentUserCards) {
-        // dealCard(CARD_FILE[card.color][card.value]); -- change by troy
         dealCard(card)
+        numCards++;
     }
+    root.style.setProperty('--numCards', numCards);
+    numCards = 0;
 
-    Object.keys(gamePlayers).forEach(key => {
-        if (gamePlayers[key] !== "") {
-            const currentOpponentCards = gameState?.cards.filter(card => {
-                return card.user_id === gamePlayers[key];
-            })
-            for (const card of currentOpponentCards) {
-                dealOpponentCard(Object.keys(gamePlayers).find(key => object[key] === value));
-            }
+    for (const [key, value] of opponentMap){
+        const currentOpponentCards = gameState?.cards.filter(card => {
+            return card.user_id === key;
+        });
+        for (const card of currentOpponentCards){
+            dealOpponentCard(value);
+            numCards++;
         }
-    });
+        root.style.setProperty('--'+value+'Cards', numCards);
+        numCards = 0;
+    }
 
     const discardPile = gameState?.cards.filter(card => {
         return card.location === "DISCARD"
@@ -155,7 +172,7 @@ socket.on('game_event', (gameEvent) => {
             break;
         //Additional keys: user_id
         case "DEALT_CARD":
-            visualizeDealtCard(gameEvent?.user_id);
+            //visualizeDealtCard(gameEvent?.user_id);
             break;
         case "GAME_DELETED":
             //?? what do we display here
@@ -176,22 +193,20 @@ socket.on('game_event', (gameEvent) => {
 
 //TODO
 //direction of play matters
-function addPlayer(user_id) {
-  for (const opponent in gamePlayers) {
-    if (!gamePlayers[opponent]) {
-      gamePlayers[opponent] = user_id;
-      break;
-    }
-  }
+function showOpponent(key, username) {
+    opponentLocations[key].style.visibility = "visible";
+    const opName = document.getElementById(opponentLocations[key].id + "Name");
+    opName.style.visibility = "visible";
+    opName.innerHTML = username;
 }
 
-function removePlayer(user_id) {
-  for (const opponent in gamePlayers) {
-    if (gamePlayers[opponent] === user_id) {
-      gamePlayers[opponent] = '';
-      break;
+function hideOpponent(user_id) {
+    for (const opponent in opponents) {
+        if (opponents[opponent] === user_id) {
+            opponents[opponent] = '';
+            break;
+        }
     }
-  }
 }
 
 let lastDeckCard;
@@ -200,165 +215,143 @@ replenishDeck();
 //TODO
 //get number of cards reshuffled (maybe from DECK_RESHUFFLED io event)
 function replenishDeck(deckSize) {
-  if (!deckSize) {
-    deckSize = 80;
-    //TODO
-    //based on 7 cards dealt to FOUR players
-    //get number of players? or localize a value
-  }
+    if (!deckSize) {
+        deckSize = 80;
+        //TODO
+        //based on 7 cards dealt to FOUR players
+        //get number of players? or localize a value
+    }
 
-  for (let i = 0; i < deckSize; i++) {
-    let elem = document.getElementsByClassName('deckContainer').item(0);
-    let newCard = document.createElement('div');
-    newCard.classList.add('card', 'deckCard');
-    elem.appendChild(newCard);
-  }
-  lastDeckCard = document
-    .getElementsByClassName('deckContainer')
-    .item(0).lastChild;
-  lastDeckCard.addEventListener('click', takeAndDeal, false);
+    for (let i = 0; i < deckSize; i++) {
+        let elem = document.getElementsByClassName('deckContainer').item(0);
+        let newCard = document.createElement('div');
+        newCard.classList.add('card', 'deckCard');
+        elem.appendChild(newCard);
+    }
+    lastDeckCard = document
+        .getElementsByClassName('deckContainer')
+        .item(0).lastChild;
+    lastDeckCard.addEventListener('click', takeAndDeal, false);
 }
 
 function takeAndDeal() {
-  lastDeckCard.remove();
-  lastDeckCard = document
-    .getElementsByClassName('deckContainer')
-    .item(0).lastChild;
-  dealCard();
-  lastDeckCard.addEventListener('click', takeAndDeal, false);
+    lastDeckCard.remove();
+    lastDeckCard = document
+        .getElementsByClassName('deckContainer')
+        .item(0).lastChild;
+    dealCard();
+    lastDeckCard.addEventListener('click', takeAndDeal, false);
 }
-
-/**
- *             | |
- *             | |
- *             | |
- *            \   /
- *              ∨
- */
 
 function dealCard(card) {
 
-  let elem = document.getElementById('myHand');
-  let newCard = document.createElement('div');
-  newCard.classList.add('card', 'myCard');
-  newCard.setAttribute('id', `${card.card_id}`);
-  newCard.style.backgroundImage =
-    'url(' + CARD_FILE[card.color][card.value] + ')';
-  if (card.color === 'BLACK') {
-    newCard.addEventListener(
-      'click',
-      function () {
-        window.location.href = '#modal';
-        wildcard(newCard);
-      },
-      false
-    );
-  } else {
-    newCard.addEventListener(
-      'click',
-      function () {
-        playCard(newCard, null);
-      },
-      false
-    );
-  }
-  elem.appendChild(newCard);
+    let elem = document.getElementById('myHand');
+    let newCard = document.createElement('div');
+    newCard.classList.add('card', 'myCard');
+    newCard.setAttribute('id', `${card.card_id}`);
+    newCard.style.backgroundImage =
+        'url(' + CARD_FILE[card.color][card.value] + ')';
+    if (card.color === 'BLACK') {
+        newCard.addEventListener(
+            'click',
+            function () {
+                window.location.href = '#modal';
+                wildcard(newCard);
+            },
+            false
+        );
+    } else {
+        newCard.addEventListener(
+            'click',
+            function () {
+                playCard(newCard, null);
+            },
+            false
+        );
+    }
+    elem.appendChild(newCard);
 }
 
-function dealOpponentCard(user_id) {
-  const key = Object.keys(gamePlayers).find(
-    (user_id) => obj[user_id] === value
-  );
-  const elem = getElementById(key);
-  let newCard = document.createElement('div');
-  newCard.classList.add('card', key + 'card');
+function dealOpponentCard(opponentPosition) {
+    let newCard = document.createElement('div');
+    newCard.classList.add('card', opponentPosition + 'Card');
+    opponentLocations[opponentPosition].appendChild(newCard);
 }
 
-/**
- *             | |
- *             | |
- *             | |
- *            \   /
- *              ∨
- */
 
 //TODO
 //add timeout to play another card
 function playCard(elem, color) {
-  //TODO
-  const num = parseInt(elem.id);
-  const query = `/api/games/${gameId}/play-card`;
+    //TODO
+    const num = parseInt(elem.id);
+    const query = `/api/games/${gameId}/play-card`;
 
-  let request;
+    let request;
 
-  if (color != null) {
-    while (wildcardButtonContainer.firstChild) {
-      wildcardButtonContainer.removeChild(wildcardButtonContainer.firstChild);
+    if (color != null) {
+        while (wildcardButtonContainer.firstChild) {
+            wildcardButtonContainer.removeChild(wildcardButtonContainer.firstChild);
+        }
+        request = {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                card_id: num,
+                chosen_wildcard_color: color,
+            }),
+        };
+    } else {
+        request = {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                card_id: num,
+            }),
+        };
     }
-    request = {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        card_id: num,
-        chosen_wildcard_color: color,
-      }),
-    };
-  } else {
-    request = {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        card_id: num,
-      }),
-    };
-  }
 
-  fetch(query, request);
+    fetch(query, request);
 }
 
 function wildcard(card) {
-  const redButton = document.getElementById('redbttn');
-  const blueButton = document.getElementById('bluebttn');
-  const yellowButton = document.getElementById('yellowbttn');
-  const greenButton = document.getElementById('greenbttn');
+    const redButton = document.getElementById('redbttn');
+    const blueButton = document.getElementById('bluebttn');
+    const yellowButton = document.getElementById('yellowbttn');
+    const greenButton = document.getElementById('greenbttn');
 
-  redButton.addEventListener('click', function () {
-    console.log('RED picked');
-    playCard(card, 'RED');
-    window.location.href = '#';
-  });
-  blueButton.addEventListener('click', function () {
-    console.log('BLUE picked');
-    playCard(card, 'BLUE');
-    window.location.href = '#';
-  });
-  yellowButton.addEventListener('click', function () {
-    console.log('YELLOW picked');
-    playCard(card, 'YELLOW');
-    window.location.href = '#';
-  });
-  greenButton.addEventListener('click', function () {
-    console.log('GREEN picked');
-    playCard(card, 'GREEN');
-    window.location.href = '#';
-  });
+    redButton.addEventListener('click', function () {
+        console.log('RED picked');
+        playCard(card, 'RED');
+        window.location.href = '#';
+    });
+    blueButton.addEventListener('click', function () {
+        console.log('BLUE picked');
+        playCard(card, 'BLUE');
+        window.location.href = '#';
+    });
+    yellowButton.addEventListener('click', function () {
+        console.log('YELLOW picked');
+        playCard(card, 'YELLOW');
+        window.location.href = '#';
+    });
+    greenButton.addEventListener('click', function () {
+        console.log('GREEN picked');
+        playCard(card, 'GREEN');
+        window.location.href = '#';
+    });
 }
 
-/**
- *             | |
- *             | |
- *             | |
- *            \   /
- *              ∨
- */
 
 function discardPileCard(discardPile) {
+    if(gameStarted === false) {
+        return;
+    }
     let degreeTracker = 0;
     for (const card of discardPile) {
         let elem = document.getElementsByClassName("discard").item(0);
